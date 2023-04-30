e9ddb5c1e6397fecfeeec0ba4e1078119292a57b
macihasa <macihasa@gmail.com>
2023-04-30 12:41:45 +0200

    Added base of user model with Register and Update and get methods. Close #7

---
+++ backend/pkg/models/userModel.go
@@ -0,0 +1,61 @@
+package models
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type UserModel struct {
+	ID       primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserName string             `json:"username" bson:"username"`
+	PassWord string             `json:"password" bson:"password"`
+	Friends  []string           `json:"friends,omitempty" bson:"friends,omitempty"`
+}
+
+func (u UserModel) Register() (userid interface{}) {
+	var db = MongoDBClient.Database("chat")
+	collection := db.Collection("users")
+
+	res, err := collection.InsertOne(context.TODO(), bson.D{
+		{Key: "username", Value: u.UserName},
+		{Key: "password", Value: u.PassWord},
+		{Key: "friends", Value: u.Friends},
+	})
+	if err != nil {
+		log.Println("Failed creating user: ", err)
+		return
+	}
+	log.Println("Created user in DB: ", res)
+	return res.InsertedID
+}
+
+func (u UserModel) UpdateByID() {
+	var db = MongoDBClient.Database("chat")
+	collection := db.Collection("users")
+
+	res, err := collection.UpdateOne(context.TODO(), bson.D{{"_id", u.ID}}, bson.D{{"$set", u}})
+	if err != nil {
+		log.Println("Failed updating user: ", err)
+		return
+	}
+
+	log.Println("Updated user: ", res)
+
+}
+
+func (u UserModel) GetByID() {
+	var db = MongoDBClient.Database("chat")
+	collection := db.Collection("users")
+
+	res := collection.FindOne(context.TODO(), u)
+	var user UserModel
+	err := res.Decode(&user)
+	if err != nil {
+		log.Println("Couldnt decode res :", err)
+	}
+	log.Println(user)
+
+}
